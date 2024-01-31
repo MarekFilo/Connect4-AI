6b58536bbe42f0ed21cc8f889d06ede59e2c7734
MarekFilo <marekx.filo@gmail.com>
2024-01-31 15:51:13 +0100

    gameloop

--- src/game_logic/connect4.py
+++ src/game_logic/connect4.py
@@ -11,21 +11,21 @@ class Connect4:
     def __init__(self):
         self.board = np.zeros((6, 7), dtype=int)
         self.num_of_moves = 0
-        self.player_one = Player(1)
-        self.player_two = Player(2)
 
     def clear_board(self):
         self.board = np.zeros((6, 7), dtype=int)
         self.num_of_moves = 0
 
-    def make_move(self, column: int, player: Player) -> bool:
-        if self.is_valid_move(column):
-            row = self.get_next_row(column)
-            self.board[row][column] = player.number
-            self.num_of_moves += 1
-            return True
-        else:
-            return False
+    def make_move(self, column: int, player: Player):
+        if not self.is_valid_move(column):
+            raise ValueError(f"Column {column} is already full.")
+
+        row = self.get_next_row(column)
+        self.board[row][column] = player.number
+        self.num_of_moves += 1
+
+    def is_board_full(self) -> bool:
+        return self.num_of_moves == 42
 
     def get_next_row(self, column: int) -> int:
         for row in range(5, -1, -1):
@@ -37,6 +37,30 @@ class Connect4:
 
     def starting_player(self, players: [Player]) -> Player:
         return np.random.choice(players)
+
+    def initiate_game(self):
+        self.player_one = Player(1)
+        self.player_two = Player(2)
+
+        self.current_player = self.starting_player([self.player_one, self.player_two])
+
+        while not self.is_board_full():
+            column = (
+                int(input(f"Player {self.current_player.number}, choose a column: "))
+                - 1
+            )
+
+            try:
+                self.make_move(column, self.current_player)
+
+                print(self.board)
+                self.current_player = (
+                    self.player_one
+                    if self.current_player == self.player_two
+                    else self.player_two
+                )
+            except ValueError as e:
+                print(f"Error: {e}")
 
 
 if __name__ == "__main__":
